d712996a1cdd6da3f7ec9a6cd06cc1727f933dd5
yash gehlot <yashgehlot1233@gmail.com>
2026-03-14 00:30:02 +0530

    styling Components

--- firstProject/src/components/ExpenseItem.jsx
+++ firstProject/src/components/ExpenseItem.jsx
@@ -1,11 +1,12 @@
+import './ExpenseItem.css'
 export default function ExpenseItem() {
   return (
-    <>
+    <div className='expense-item'>
       <div>March 28th 2021</div>
       <div>15 August 2023</div>
-      <div>Delhi</div>
-      <h2>Book</h2>
-      <div>$10</div>
-    </>
+      <div className='expense-item__location'>Delhi</div>
+      <h2 className='expense-item__description'>Book</h2>
+      <div className='expense-item__price'>$10</div>
+    </div> 
   )
 }
