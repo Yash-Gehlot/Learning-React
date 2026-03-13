d626eb652121fe27807f5b1109ac6c3dddf345f8
yash gehlot <yashgehlot1233@gmail.com>
2026-03-14 00:35:11 +0530

    java Script inside jsx

--- firstProject/src/components/ExpenseItem.jsx
+++ firstProject/src/components/ExpenseItem.jsx
@@ -1,12 +1,23 @@
-import './ExpenseItem.css'
-export default function ExpenseItem() {
+// Write yoour code at relevant places in the code below:
+
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  const expenseDate = new Date(2023, 7, 15).toISOString();
+  const expenseTitle = "Insurance";
+  const expenseAmount = 50;
+  const expenseLocation = "Bangalore";
+
   return (
-    <div className='expense-item'>
-      <div>March 28th 2021</div>
-      <div>15 August 2023</div>
-      <div className='expense-item__location'>Delhi</div>
-      <h2 className='expense-item__description'>Book</h2>
-      <div className='expense-item__price'>$10</div>
-    </div> 
-  )
+    <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className="expense-item__location">{expenseLocation}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
+
+export default ExpenseItem;
