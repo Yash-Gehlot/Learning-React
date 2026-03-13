09085d9c8f2d9e8c1d5f061f60e76ce527ec7b30
yash gehlot <yashgehlot1233@gmail.com>
2026-03-13 22:06:35 +0530

    writing Complex JSX

--- firstProject/src/components/ExpenseItem.jsx
+++ firstProject/src/components/ExpenseItem.jsx
@@ -1,9 +1,11 @@
 export default function ExpenseItem() {
   return (
-    <div>
-      <p>Food - $5</p>
-      <p>Petrol - $20</p>
-      <p>Movie - $10</p>
-    </div>
+    <>
+      <div>March 28th 2021</div>
+      <div>15 August 2023</div>
+      <div>Delhi</div>
+      <h2>Book</h2>
+      <div>$10</div>
+    </>
   )
 }
